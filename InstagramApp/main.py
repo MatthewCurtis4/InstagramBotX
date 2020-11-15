54f270bd3ba471a35c9145ee6db9a5ad00345d77
RealMCCurtis <mcurtisbusinesses@gmail.com>
2020-11-15 18:09:10 -0500

    Fixing repo

---
+++ InstagramApp/main.py
@@ -0,0 +1,10 @@
+
+
+from selenium import webdriver
+
+class InstagramBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        driver.get("https://youtube.com")
+
+InstagramBot()
