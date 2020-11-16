5da44d5aca6c3ef56abe8bf7bcb4bb681c749ee7
MatthewCurtis4 <mcurtisbusinesses@gmail.com>
2020-11-15 20:01:01 -0500

    added dependancies and setting it up so I can lauch it to chrome

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+'''
+Setting up Chrome options for use - necessary for chrome to be used in Repl.it
+'''
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+
 
 class InstagramBot:
     def __init__(self):
+
         self.driver = webdriver.Chrome()
-        driver.get("https://youtube.com")
+        self.driver.get('https://www.instagram.com')
 
 InstagramBot()
